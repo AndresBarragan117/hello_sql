ce049bfea63f39d9b87f9d363c8aebb5f6dbb9d1
Andres <u20222208879@usco.edu.co>
2024-01-29 18:09:40 -0500

    como se usa el distinc

---
+++ Statemens/distinc.sql
@@ -0,0 +1,3 @@
+SELECT distinct * FROM users;   --aqui se seleciona todas las columnas de la tabla de users pero sin repetir los datos--
+
+SELECT distinct age FROM users;  --aqui se seleciona solo la columna age de la tabla users pero sin repetir los datos--
