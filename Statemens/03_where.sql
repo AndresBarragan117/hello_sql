4659a6ca1ffa40e10c1b3b13b3121ce6083d5c2a
Andres <u20222208879@usco.edu.co>
2024-01-29 18:19:44 -0500

    como se usa el where en BD

--- Statemens/03_where.sql
+++ Statemens/03_where.sql
@@ -1,1 +1,7 @@
 SELECT * FROM users WHERE age=20; --aqui se seleciona todas las columnas de la tabla de users donde la edad sea 20--
+
+SELECT name FROM users WHERE age=20; --aqui se seleciona solo la columna name de la tabla users donde la edad sea 20--
+
+SELECT DISTINCT age FROM users WHERE age=20; --aqui se seleciona solo la columna age de la tabla users donde la edad sea 20 pero sin repetir los datos--
+
+SELECT DISTINCT name FROM users WHERE age=20; --aqui se seleciona solo la columna name de la tabla users donde la edad sea 20 pero sin repetir los datos--
