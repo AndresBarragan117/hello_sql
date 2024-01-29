17d6fd1e1c3fceb8a4298d720f30ca47c813e793
Andres <u20222208879@usco.edu.co>
2024-01-29 17:53:41 -0500

    formas de usar el selec

---
+++ Statemens/Select.sql
@@ -0,0 +1,5 @@
+SELECT * FROM users;  --aqui se seleciona todas las columnas de la tabla de users--
+
+SELECT age FROM users;  --aqui se seleciona solo la columna age de la tabla users--
+
+SELECT user_id, name FROM users;  --aqui se seleciona solo la columna user_id y name de la tabla users--
