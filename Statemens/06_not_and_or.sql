2871c4fd590abf9d129df0292eb4048da1e71260
Andres <u20222208879@usco.edu.co>
2024-01-29 18:49:07 -0500

    como se usa el AND, OR, NOT, para las cosultas

--- Statemens/06_not_and_or.sql
+++ Statemens/06_not_and_or.sql
@@ -1,2 +1,5 @@
 SELECT * FROM users WHERE NOT email = "andres@gmail.com"; --aqui se seleciona todas las columnas de la tabla de users donde el email no sea andres@gmail.com--
 
+SELECT * FROM users WHERE NOT email = "loren@gmail.com" AND age = 20; --aqui se seleciona todas las columnas de la tabla de users donde el email no sea loren@gmail.com y la edad sea 20--
+
+SELECT * FROM users WHERE NOT email = "andres@gmail.com" OR age = 20; --aqui se seleciona todas las columnas de la tabla de users donde el email no sea andres@gmail.com o la edad sea 20--
